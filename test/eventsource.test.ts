bc087f0de9dbeb0ea3e6dd88af9412d77ce93aaa
Paul O'Fallon <pofallon@users.noreply.github.com>
2020-06-07 20:32:57 -0400

    Add small test

---
+++ test/eventsource.test.ts
@@ -0,0 +1,15 @@
+import { expect as expectCDK, haveResource } from '@aws-cdk/assert'
+import * as cdk from '@aws-cdk/core'
+import * as activity from '../lib/index'
+
+
+
+test('EventSource Created', () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, "TestStack")
+  // WHEN
+  // tslint:disable-next-line: no-unused-expression
+  new activity.EventSource(stack, 'TestEventSource')
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::Events::EventBus"))
+})
