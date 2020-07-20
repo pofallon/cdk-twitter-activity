f29d8b37043c44025c0013c624f388b362b51b04
Paul O'Fallon <pofallon@users.noreply.github.com>
2020-07-19 20:34:59 -0400

    Output the EventBus ARN

--- examples/typescript/lib/twitter-activity-stack.ts
+++ examples/typescript/lib/twitter-activity-stack.ts
@@ -2,11 +2,19 @@ import * as cdk from '@aws-cdk/core';
 import * as activity from '../../../lib/index'
 
 export class TwitterActivityStack extends cdk.Stack {
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
+    const eventSource = new activity.EventSource(this, 'ActivityEventSource')
+    
+    // Share the ARN of this EventBus so other stacks can leverage it
     // tslint:disable-next-line: no-unused-expression
-    new activity.EventSource(this, 'ActivityEventSource')
+    new cdk.CfnOutput(this, 'EventBusOutput', {
+      description: 'Twitter Activity EventBus ARN',
+      exportName: 'twitter-activity-eventbus-arn',
+      value: eventSource.eventBus.eventBusArn
+    })
 
   }
 }
